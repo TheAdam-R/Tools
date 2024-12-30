5a56b5d2ebcecc9a461dfc4598bcb498aefed0fb
Adam Rutecki <arutecki2@gmail.com>
2024-12-30 00:16:16 -0600

    fixed comments

--- ClassDirMK.ps1
+++ ClassDirMK.ps1
@@ -1,16 +1,16 @@
-#   Class Directory Maker - Ver 1 Windows PowerShell
+#   Class Directory Maker - Ver 1 - Windows PowerShell
 #   Adam Rutecki - August 2024
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 #   Makes Folders for classes so I dont have to ;)
-#   Use: in powershell terminal run $"./ClassDirMK" "[Class Abreviation]"
+#   In powershell terminal run: $"./ClassDirMK" "[Class Abreviation]"
 
 
 Write-Host $args[0]
 $name = $args[0]            #takes arg0 as folder name
 
 #   DEV - Switch comment when testing to check results faster
-#$location = "~\Documents"  #desired folder origin path
-$location = "."             #testing value
+$location = "~\Documents"  #desired folder origin path
+#$location = "."             #testing value
 
 #   Creating folders
 mkdir $location\$name\Readings
